8a2aab1a9285904d26e533461716fa1b6f46010e
“JennyOh1004” <ohseulki1004@gmail.com>
2019-09-10 01:38:03 +0000

    Initial commit

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,19 @@
 class Comment < ApplicationRecord
 	belongs_to :user
 	belongs_to :place
+
+
+	RATINGS = {
+		'one star': '1_star',
+		'two star': '2_stars',
+		'three star': '3_stars',
+		'four star': '4_stars',
+		'five stars': '5_stars'
+		 #user will see   #stored in database
+	}
+
+	def humanized_rating
+		RATINGS.invert[self.rating]
+	end	
 end
+
