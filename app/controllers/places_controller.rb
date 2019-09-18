2155b6b1be9dcff85820d4c5c7f7505771898cc3
“JennyOh1004” <ohseulki1004@gmail.com>
2019-09-18 00:26:07 +0000

    Initial commit

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -3,8 +3,7 @@ class PlacesController < ApplicationController
 
 
 	def index
-		@places = Place.all
-		@places = Place.paginate(:page => params[:page], :per_page => 30)
+		@places = Place.all.paginate(:page => params[:page], :per_page => 30)
 
 	end
 
@@ -25,7 +24,8 @@ class PlacesController < ApplicationController
 	def show
 		@place = Place.find(params[:id])
 		@comment = Comment.new
-	end
+		@photo = Photo.new
+  end
 
 	def edit
 		@place = Place.find(params[:id])
